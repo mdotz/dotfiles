2b81332607b303e9f8fff12ed23e218c9c8d3a73
mdotz <marcin.x.zielinski@gmail.com>
2019-05-26 19:17:02 +0200

    startx on boot in fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,12 @@
 . "$HOME/.config/fish/functions/aliases.fish"
+
+# Start X at login
+if status --is-login
+  if test -z "$DISPLAY" -a $XDG_VTNR = 1
+    exec startx
+  end
+end
+
 xset r rate 250 40
+
 set -g fish_prompt_pwd_dir_length 20
